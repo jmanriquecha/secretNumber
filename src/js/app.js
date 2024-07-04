bc35c564521ba3e5f2edd2fe9a185d960adcb14c
Jorge Manrique <georgemanc.28@gmail.com>
2024-07-04 23:00:07 +0000

    Se agrega contenido desde JS

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// Declaración de variables
+let titulo = document.getElementById('titulo');
+let parrafo = document.getElementById('parrafo');
+
+// Agregamos texto al h1 'titulo'
+titulo.innerHTML = 'Juego del número secreto!';
+
+// Agregamos texto al elemento p 'parrafo'
+parrafo.innerHTML = 'Ingrese un número del 1 al 10';
