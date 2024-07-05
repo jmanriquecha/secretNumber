c81fb6f1f1a1b39f117700b86950e5bea9ec0a08
Jorge Manrique <georgemanc.28@gmail.com>
2024-07-05 20:54:28 +0000

    Se crea función para validar intentos

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 // Declaración de variables
 let numeroSecreto = generaNumeroAleatorio(1, 10);
+const btnStart = document.getElementById('btnStart');
+
+// a la escucha de eventos
+btnStart.addEventListener('click', validaIntento);
 
 // Declaramos las funciones
 
@@ -17,6 +21,16 @@ function asignaTextoElementoId(id, texto) {
  */
 function generaNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min) + min) + 1;
+}
+
+function validaIntento() {
+    let valorUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    // Validaciones
+    if (numeroSecreto === valorUsuario) {
+        console.log('Ganaste');
+    }
+
 }
 
 // Agregamos texto al h1 'titulo'
